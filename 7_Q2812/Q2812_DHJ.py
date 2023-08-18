fdb532c92c24fb8f77ca1c7646f46119f248639e
Designated-Hitter <dosadola@gmail.com>
2023-08-18 21:30:25 +0900

    [ADD - DHJ]Q2812 solved

--- 7_Q2812/Q2812_DHJ.py
+++ 7_Q2812/Q2812_DHJ.py
@@ -1,8 +1,16 @@
 import sys
 N, K = map(int, sys.stdin.readline().split())
 
-print(N)
-print(K)
+numbers = list(map(int, sys.stdin.readline().strip()))
 
-num = str(sys.stdin.readline)
-print(num)
+stack = []
+
+for number in numbers:
+    while stack and stack[-1] < number and K > 0:
+        stack.pop()
+        K -= 1
+    stack.append(number)
+if K > 0:
+    print(''.join(list(map(str, stack[:-K]))))
+else:
+    print(''.join(list(map(str, stack))))
