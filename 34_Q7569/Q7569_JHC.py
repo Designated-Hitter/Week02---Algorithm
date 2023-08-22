81063f206599f28c281fd0213d5d167301852ad4
jongchang <hokhmah91@naver.com>
2023-08-22 20:00:47 +0900

    [ADD]:jonghyeon

--- 34_Q7569/Q7569_JHC.py
+++ 34_Q7569/Q7569_JHC.py
@@ -1,1 +1,71 @@
+import heapq
 import sys
+input = sys.stdin.readline
+
+'''
+[[[ 0,-1, 0, 0, 0 ],
+  [-1,-1, 0, 1, 1 ],
+  [ 0, 0, 0, 1, 1 ]],
+
+ [[ 0,-1, 0, 0, 0 ],
+  [-1,-1, 0, 1, 1 ],
+  [ 0, 0, 0, 1, 1 ]]]
+       
+       h  n  m
+graph[0][0][0]
+'''
+
+
+m, n, h = map(int, input().split())     # m:가로 n:세로 h:높이
+
+graph = [[list(map(int, input().split()))  for _ in range(n)] for _ in range(h)]
+
+visited = [[[False]*m for _ in range(n)] for _ in range(h)]
+
+queue = deque =()
+
+dx = [-1,1,0,0,0,0]
+dy = [0,0,-1,1,0,0]
+dz = [0,0,0,0,-1,1]
+
+answer = 0
+
+def bfs():
+    while queue:
+        x, y, z = queue.popleft()
+
+        for i in range(6):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = z + dz[i]
+
+            if (nx < 0) or (nx >= h) or (ny < 0) or (ny >= n) or (nz < 0) or (nz >= m):
+                continue
+
+            if graph[nx][ny][nz] == 0 and visited[nx][ny][nz] == False:
+                queue.append((nx, ny, nz))
+                graph[nx][ny][nz] = graph[x][y][z] + 1
+                visited[nx][ny][nz] = True
+
+# 모두 1이 아닐 경우
+for a in range(h):
+    for b in range(n):
+        for c in range(m):
+            if graph[a][b][c] == 1 and visited[a][b][c] ==0:
+                queue.append((a, b, c))
+                visited[a][b][c] = True
+
+bfs()
+
+# 토마토 확인
+for a in graph:
+    for b in a:
+        for c in b:
+            if c == 0:
+                print(-1)
+                exit(0)
+        answer = max(answer, max(b))
+
+print(answer - 1)
+
+# 어차피 모두 1이라면 0이 출력된다.
